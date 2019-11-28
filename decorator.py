eeb23b2d754afeb50dadca15a4d6e19ef801e92c
Artem <artger@ukr.net>
2019-11-28 12:57:01 +0200

    version 1

--- decorator.py
+++ decorator.py
@@ -1,11 +1,35 @@
-def hide(f):
+'''
+Module
+'''
 
-    return lambda x: 42
+
+def hide(funct):
+    '''
+
+    :param fnctn:
+    :return:
+    '''
+    def decorat(number):
+        '''
+
+        :param number:
+        :return:
+        '''
+        if number < 5:
+            number *= 2
+        number = funct(number)
+        if number % 2:
+            number += 1
+            return number
+        return number
+    return decorat
 
 
 @hide
-def f(x):
-    return x + 5
+def fnctn(number):
+    '''
 
-
-print(f(7))
+    :param x:
+    :return:
+    '''
+    return number + 5
