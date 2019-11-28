10fe0bbef4c96b393cd05041a26d8293f239927c
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-28 16:01:04 +0200

    Interesting

--- decorator.py
+++ decorator.py
@@ -1,11 +1,17 @@
-def hide(f):
+"""Calculate the result according to README.md instructions"""
 
-    return lambda x: 42
+def hide(the_func):
+    """Hide the tricky actions"""
+    def wrapper(argument):
+        """Check if initial argument is less than 5 and
+        if the final function result is eve or odd"""
+        wrapped = the_func(argument * 2 if argument < 5 else argument)
+        return wrapped if (wrapped + 5) % 2 else wrapped + 1
+
+    return wrapper
 
 
 @hide
-def f(x):
-    return x + 5
-
-
-print(f(7))
+def outer_function(argument):
+    """Add 5 to argument argument"""
+    return argument + 5
