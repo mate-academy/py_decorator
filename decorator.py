cfb333e37994546f142a8a999eb405feacea0729
vlysenok <tina.lysenok@gmail.com>
2019-11-28 13:28:23 +0200

    hw9_py_decorator

--- decorator.py
+++ decorator.py
@@ -1,11 +1,35 @@
-def hide(f):
+"""
+module decorator
+"""
+from typing import Callable, Any
 
-    return lambda x: 42
+
+def hide(function: Callable[[Any], Any]) -> Callable[[Any], Any]:
+    """
+    Decorator for the function with one argument num that:
+    if num < 5 changes it on 2 * num
+    if the result is odd returns result + 1
+    :param function: Callable[[Any], Any]
+    :return: Callable[[Any], Any]
+    """
+
+    def wrapper(num):
+        if num < 5:
+            num *= 2
+        result = function(num)
+        return result + 1 if result % 2 else result
+
+    return wrapper
 
 
 @hide
-def f(x):
-    return x + 5
+def func(num: int) -> int:
+    """
+    Increase the number by 5.
+    :param num: int
+    :return: int
+    """
+    return num + 5
 
 
-print(f(7))
+print(func(7))
