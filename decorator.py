4999b437bab067058b37340252740fd72029a578
Andrii <andrewsotnikov88@gmail.com>
2019-12-01 21:31:04 +0200

    Decorator custom implementation

--- decorator.py
+++ decorator.py
@@ -1,11 +1,18 @@
-def hide(f):
+"""Create a decorator for the function with one argument x
+that: if x < 5 changes it on 2*x if the result is odd returns result + 1"""
 
-    return lambda x: 42
+
+def hide(function):
+    """decorator custom implementation"""
+    def wrapper(number):
+        if number < 5:
+            number *= 2
+        total = function(number)
+        return total + 1 if total % 2 else total
+    return wrapper
 
 
 @hide
-def f(x):
-    return x + 5
-
-
-print(f(7))
+def func(elem):
+    """decorated method"""
+    return elem + 5
